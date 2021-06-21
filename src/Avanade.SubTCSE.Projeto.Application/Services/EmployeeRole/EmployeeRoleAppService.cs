6275aa11dd76083b7873f131685b65383c478fd5
Eduardo Lopes Gomes <eduaardo.lopes.gomes@avanade.com>
2021-06-21 09:02:45 -0300

    aula 5

---
+++ src/Avanade.SubTCSE.Projeto.Application/Services/EmployeeRole/EmployeeRoleAppService.cs
@@ -0,0 +1,13 @@
+﻿using Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole;
+using Avanade.SubTCSE.Projeto.Application.Interfaces.EmployeeRole;
+
+namespace Avanade.SubTCSE.Projeto.Application.Services.EmployeeRole
+{
+    class EmployeeRoleAppService : IEmployeeRoleAppService
+    {
+        public void AddemplyeeRole(EmployeeRoleDto employeeRoleDto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

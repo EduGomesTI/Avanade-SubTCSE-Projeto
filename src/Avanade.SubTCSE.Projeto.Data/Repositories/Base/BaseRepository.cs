1a96e47d7492031f39d820ea23cee6871ab44a45
Eduardo Lopes Gomes <eduaardo.lopes.gomes@avanade.com>
2021-07-07 08:48:29 -0300

    BaseRepository

---
+++ src/Avanade.SubTCSE.Projeto.Data/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Avanade.SubTCSE.Projeto.Domain.Aggregates;
+using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+
+namespace Avanade.SubTCSE.Projeto.Data.Repositories.Base
+{
+    public abstract class BaseRepository<TEntity, Tid>
+       : IBaseRepository<TEntity, Tid> where TEntity : BaseEntity<Tid>
+    {
+        public virtual async Task<TEntity> Add(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<TEntity> FindById(Tid id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Interfaces/Repositories/IEmployeeRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Repositories
+{
+    public interface IEmployeeRoleRepository : IBaseRepository<Entities.EmployeeRole, string>
+    {
+       
+    }
+}
